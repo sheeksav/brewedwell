bd414d19ec6ba96dacb78db16529c1d753c9ceb4
sheeksav <andy.sheeks@gmail.com>
2014-05-13 10:34:49 -0400

    Added attr info to LoginForm and fixed a typo.

--- engine/forms.py
+++ engine/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-	email = forms.EmailField()
-	password = forms.CharField
+	email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control', 'type': 'email'}))
+	password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}))
 
 	def clean_email(self):
 		email = self.cleaned_data.get('email')
 
 		if not User.objects.filter(email=email).exists():
-			raise form.ValidationError('Email address not found')
+			raise forms.ValidationError('Email address not found')
 
 		return email
 
