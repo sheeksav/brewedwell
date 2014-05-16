8eb10e08eb16f324d00e7b5a1bd18bcd8225be35
sheeksav <andy.sheeks@gmail.com>
2014-05-16 10:05:54 -0400

    Added password complexity.

--- engine/forms.py
+++ engine/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from passwords.fields import PasswordField
+
 class LoginForm(forms.Form):
 	email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control', 'type': 'email'}))
 	password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}))
@@ -16,7 +18,7 @@ class LoginForm(forms.Form):
 
 class SignupForm(forms.Form):
 	email = forms.EmailField(required=True, max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control', 'type': 'email'}))
-	password = forms.CharField(required=True, max_length=200, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}))
+	password = PasswordField(required=True, max_length=200, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}))
 	first_name = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}))
 	last_name = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control'}))
 		
