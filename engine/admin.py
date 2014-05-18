c474771ca9d490000803192888e4596bc8865a8e
sheeksav <andy.sheeks@gmail.com>
2014-05-17 23:21:09 -0400

    Now you can see History model in Admin.

--- engine/admin.py
+++ engine/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from engine.models import Brewery, Beer, Style
+from engine.models import Brewery, Beer, Style, History
 
 # Register your models here.
 admin.site.register(Brewery)
 admin.site.register(Beer)
 admin.site.register(Style)
+admin.site.register(History)
